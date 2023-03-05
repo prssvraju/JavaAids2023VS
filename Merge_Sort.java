80e7d1b70efee36afc5f4731a5b4da2f1f7b201e
prssvraju <prssvraju2@gmail.com>
2023-03-05 22:14:41 +0530

    Merge updated

--- Merge_Sort.java
+++ Merge_Sort.java
@@ -6,13 +6,22 @@ class Merge_sort {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array");
         int n = sc.nextInt();
-        int a[] = new int[5];
+        int a[] = new int[n];
         System.out.println("Enter the elements of the array");
         for (int i = 0; i < n; i++) {
             a[i] = sc.nextInt();
         }
-       
-        merge(a, 0, n);
+
+        System.out.println("\nElements Before Sorting a");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i]+"\t");
+        }
+        merge(a, 0, n-1);
+
+        System.out.println("\nElements After Sorting a");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i]+"\t");
+        }
     }
 
     public static void merge(int a[], int low, int high) {
