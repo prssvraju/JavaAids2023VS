498aa80c7553c3b9e599108476d1b4d8db0ce628
prssvraju <prssvraju2@gmail.com>
2023-03-15 09:33:34 +0530

    update

---
+++ Car.java
@@ -0,0 +1,21 @@
+//Illustration of static binding  
+class Vehicle
+{ 
+    public void display()  
+    {
+        System.out.println("vehicle details");  
+    } 
+} 
+class Car extends Vehicle
+{// inheriting super class  
+    public void color()
+    {
+        System.out.println("Color of car is Black");  
+    }  
+    public static void main (String[]args)
+    { 
+        Car c= new Car();// creating an object 
+        c.color();  
+    }  
+} 
+
