f595f24a63da6c6b14b03545a46c6936b020c9a4
prssvraju <prssvraju2@gmail.com>
2023-04-26 09:33:14 +0530

    ThreadStop

---
+++ Thread/ThreadStop.java
@@ -0,0 +1,60 @@
+package Thread;
+
+class Thread1 extends Thread
+{
+    public void run()
+    {
+        try{
+            sleep(1000);
+        }
+        catch(InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
+class Thread2 extends Thread
+{
+    public void run()
+    {
+        try{
+            sleep(2000);
+        }
+        catch(InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
+public class ThreadStop {
+    public static void main(String args[])
+    {
+        Thread1 t1 = new Thread1();
+        Thread2 t2 = new Thread2();
+        t1.start();
+        try
+        {
+            t1.join();
+        }
+        catch(InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+
+        t2.start();
+        System.out.println("State fo T2 "+t2.isAlive()+"State :"+t2.getState());
+        try{
+            Thread.sleep(5000);
+        }
+        catch(InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+
+        System.out.println("State fo T1 "+t1.isAlive()+"State :"+t1.getState());
+        System.out.println("Exit main Thread");
+ 
+    }
+}
